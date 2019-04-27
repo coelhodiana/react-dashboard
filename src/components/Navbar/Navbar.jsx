bd414522be90c64c8cfea9804a2719eb34054a92
Diana <coelhodiana04@gmail.com>
2019-04-26 23:48:59 -0300

    Início do projeto da Dashboard usando React.js

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+    render(){
+        return(
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="/" className="navbar-brand">DevMedia</a>
+                <button className="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span className="navbar-toggle-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mr-auto">
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
