25ddaeb614b891f9bf574ba09adc14d4e4361911
Diana <coelhodiana04@gmail.com>
2019-05-07 00:37:07 -0300

    Novos componentes navbar e summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
+import Summary from './components/Summary/Summary.jsx';
 
 class App extends Component {
   render() {
     return (
-      <Navbar/>
+      <div>
+        <Navbar/>
+        <Summary/>
+      </div>
     );
   }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href="/" className="navbar-brand">DevMedia</a>
-                <button className="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav">
-                    <span className="navbar-toggle-icon"></span>
+                <a href="/" className="navbar-brand">react dashboard</a>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mr-auto">
                     </ul>
